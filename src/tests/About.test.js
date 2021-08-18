6009c724b452c9ceb6824d3d7c0f87b5efa3a57b
Marcos Estevao <dusdoom@gmail.com>
2021-08-18 16:59:02 -0300

    requirement 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,25 @@
-test('', () => {});
-//  initial commit
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('When rendering About page', () => {
+  it('should render the right heading', () => {
+    render(<About />);
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    })).toBeInTheDocument();
+  });
+  it('should container two paragraphs with the pokedex description', () => {
+    render(<About />);
+    const firstText = screen.getByText(/this application/i);
+    const secondText = screen.getByText(/one can filter/i);
+    expect(firstText && secondText).toBeInTheDocument();
+  });
+  it('should contain the right image', () => {
+    render(<About />);
+    const expectedLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = screen.getByRole('img');
+    expect(image.src).toEqual(expectedLink);
+  });
+});
