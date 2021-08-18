c4ec9a0d235b4f43db8590ff1ef1ee1422d39841
Marcos Estevao <dusdoom@gmail.com>
2021-08-18 17:33:25 -0300

    requirement 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+function renderWithRouter(componentToRender) {
+  const customMemory = createMemoryHistory();
+  return {
+    ...render(
+      <Router history={ customMemory }>
+        {componentToRender}
+      </Router>,
+    ),
+    history: customMemory,
+  };
+}
+
+describe('when rendering <FavoritePokemon />', () => {
+  it('should show \'no favorite pokemons found\' if theres not one', () => {
+    render(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite pokemon found/)).toBeInTheDocument();
+  });
+  it('should show favorite pokemon list', () => {
+    const favPokemons = [pokemons[0]];
+    renderWithRouter(<FavoritePokemons pokemons={ favPokemons } />);
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
+});
